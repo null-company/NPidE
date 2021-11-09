980b8013cd5c2aa625c56466565172df7256515e
Roman <b111forjob@gmail.com>
2021-11-09 20:50:48 +0700

    Add compile button to statusbar

--- src/main/kotlin/ru/nsu_null/npide/ui/statusbar/StatusBar.kt
+++ src/main/kotlin/ru/nsu_null/npide/ui/statusbar/StatusBar.kt
@@ -1,13 +1,12 @@
 package org.jetbrains.codeviewer.ui.statusbar
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.Slider
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.*
 import ru.nsu_null.npide.ui.common.Settings
@@ -22,22 +21,35 @@ fun StatusBar(settings: Settings) = Box(
         .fillMaxWidth()
         .padding(4.dp)
 ) {
-    Row(Modifier.fillMaxHeight().align(Alignment.CenterEnd)) {
-        Text(
-            text = "Text size",
-            modifier = Modifier.align(Alignment.CenterVertically),
-            color = LocalContentColor.current.copy(alpha = 0.60f),
-            fontSize = 12.sp
-        )
 
-        Spacer(Modifier.width(8.dp))
+    Row(Modifier.fillMaxWidth().padding(horizontal = 4.dp), horizontalArrangement = Arrangement.SpaceBetween) {
 
-        CompositionLocalProvider(LocalDensity provides LocalDensity.current.scale(0.5f)) {
-            Slider(
-                (settings.fontSize - MinFontSize) / (MaxFontSize - MinFontSize),
-                onValueChange = { settings.fontSize = lerp(MinFontSize, MaxFontSize, it) },
-                modifier = Modifier.width(240.dp).align(Alignment.CenterVertically)
+        Button(onClick = { }, contentPadding = PaddingValues(), modifier = Modifier.align(Alignment.CenterVertically)) {
+            Text(
+                text = "Compile",
+                color = LocalContentColor.current.copy(alpha = 0.60f),
+                fontSize = 12.sp
             )
+        }
+
+        Row {
+            Text(
+                text = "Text size",
+                modifier = Modifier.align(Alignment.CenterVertically),
+                color = LocalContentColor.current.copy(alpha = 0.60f),
+                fontSize = 12.sp
+            )
+
+            Spacer(Modifier.width(8.dp))
+
+
+            CompositionLocalProvider(LocalDensity provides LocalDensity.current.scale(0.5f)) {
+                Slider(
+                    (settings.fontSize - MinFontSize) / (MaxFontSize - MinFontSize),
+                    onValueChange = { settings.fontSize = lerp(MinFontSize, MaxFontSize, it) },
+                    modifier = Modifier.width(240.dp).align(Alignment.CenterVertically)
+                )
+            }
         }
     }
 }
