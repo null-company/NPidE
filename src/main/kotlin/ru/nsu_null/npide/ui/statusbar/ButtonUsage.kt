6c437708f23a6b3979de4f34e7f1b745a79009e3
Roman <b111forjob@gmail.com>
2021-11-15 19:00:44 +0700

    Catch crash if compilation is not successful

--- src/main/kotlin/ru/nsu_null/npide/ui/statusbar/ButtonUsage.kt
+++ src/main/kotlin/ru/nsu_null/npide/ui/statusbar/ButtonUsage.kt
@@ -1,6 +1,7 @@
 package ru.nsu_null.npide.ui.statusbar
 
 import ru.nsu_null.npide.ui.editor.Editors
+import java.io.IOException
 
 class ButtonUsage(private val editors: Editors) {
     private fun fileExt(filename: String): String {
@@ -20,7 +21,12 @@ class ButtonUsage(private val editors: Editors) {
     fun usage() {
         if(fileExt(editors.openedFile.name) == "c") {
             runCommand("gcc", editors.openedFile.filepath, "-o", editors.openedFile.parentpath + "/out.out")
-            runCommand(editors.openedFile.parentpath + "/out.out")
+            try {
+                runCommand(editors.openedFile.parentpath + "/out.out")
+            } catch (e: IOException) {
+                // usually for the case of compilation failure
+                e.printStackTrace()
+            }
         } else {
             println("It's not c file")
         }
