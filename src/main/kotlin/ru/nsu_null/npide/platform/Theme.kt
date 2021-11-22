d6c1400d4e95f26ec62df9e339a015ec0c434fe2
Roman <b111forjob@gmail.com>
2021-11-22 19:58:03 +0700

    Delete platform theme

--- src/main/kotlin/ru/nsu_null/npide/platform/Theme.kt
+++
@@ -1,7 +0,0 @@
-package ru.nsu_null.npide.platform
-
-import androidx.compose.desktop.DesktopMaterialTheme
-import androidx.compose.runtime.Composable
-
-@Composable
-fun PlatformTheme(content: @Composable () -> Unit) = DesktopMaterialTheme(content = content)
