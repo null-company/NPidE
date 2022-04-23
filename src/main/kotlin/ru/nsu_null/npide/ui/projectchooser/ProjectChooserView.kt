0fb92f4d45b96ec399485af66c9e64b5f39bab7c
Roman <42515597+ghB111@users.noreply.github.com>
2022-04-23 16:14:42 +0700

    Project creation using JFileChooser

--- src/main/kotlin/ru/nsu_null/npide/ui/projectchooser/ProjectChooserView.kt
+++ src/main/kotlin/ru/nsu_null/npide/ui/projectchooser/ProjectChooserView.kt
@@ -1,18 +1,14 @@
 package ru.nsu_null.npide.ui.projectchooser
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,26 +16,32 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import ru.nsu_null.npide.platform.VerticalScrollbar
+import ru.nsu_null.npide.platform.HomeFolder
+import ru.nsu_null.npide.platform.toJavaFile
 import ru.nsu_null.npide.ui.common.AppTheme
 import ru.nsu_null.npide.ui.npide.NPIDE
 import ru.nsu_null.npide.ui.projectchooser.ProjectChooser.Project
+import javax.swing.JFileChooser
 
+@Preview
 @Composable
 fun ProjectChooserView() {
     val projectChooser = remember { ProjectChooser(DiskHomeDirectoryRepositoryManager()) }
     val availableProjects = projectChooser.availableProjects
+    val grayButtonColor = buttonColors(Color.Gray)
     Row(
-        Modifier.background(AppTheme.colors.backgroundDark)
+        Modifier.background(AppTheme.colors.backgroundDark).fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
-        Column {
-            val newProjectPath = remember { mutableStateOf("") }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
             OutlinedTextField(
-                value = newProjectPath.value,
-                onValueChange = { newProjectPath.value = it },
-                label = { Text("New project path", fontWeight = FontWeight.Bold, color = Color.Gray) },
+                value = "",
+                onValueChange = { },
+                enabled = false,
+                label = { Text("NPIDE", fontWeight = FontWeight.Bold, color = Color.Gray) },
                 textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Light),
-                modifier = Modifier.padding(10.dp),
+                modifier = Modifier.padding(30.dp),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = Color.Black,
                     unfocusedBorderColor = Color.Black,
@@ -48,45 +50,56 @@ fun ProjectChooserView() {
             )
             Button(
                 onClick = {
-                    val newProject = Project(newProjectPath.value)
+                    val newProject =
+                        Project(chooseProjectFolderDialog() ?: return@Button)
                     projectChooser.addProject(newProject)
                     NPIDE.openProject(newProject)
                 },
+                colors = grayButtonColor,
                 content = {
-                    Text("Add", color = Color.White)
+                    Text("Add new project", color = Color.White)
                 },
             )
         }
 
-        val scrollState = rememberLazyListState()
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            state = scrollState
+        // note this likely break when there are many projects, todo introduce lazycolumn
+        Column(
+            verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
-            items(availableProjects.value.size) { index ->
-                val project = availableProjects.value[index]
-                Row {
-                    Button(
-                        onClick = { NPIDE.openProject(project) },
-                        content = {
-                            Text(project.rootFolder.filepath, color = Color.White)
-                        },
-                    )
-                    Button(
-                        onClick = { projectChooser.deleteProject(project) },
-                        content = {
-                            Text("X")
-                        }
-                    )
+            for (project in availableProjects.value) {
+                Box(Modifier.fillMaxWidth(0.4f)) {
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Button(
+                            onClick = { NPIDE.openProject(project) },
+                            content = {
+                                Text(project.rootFolder.filepath, color = Color.White)
+                            },
+                            colors = grayButtonColor
+                        )
+                        Button(
+                            onClick = { projectChooser.deleteProject(project) },
+                            content = {
+                                Text("X")
+                            },
+                            colors = grayButtonColor
+                        )
+                    }
                 }
             }
         }
-
-        VerticalScrollbar(
-            Modifier.align(Alignment.CenterVertically),
-            scrollState,
-            availableProjects.value.size,
-            5.dp
-        )
     }
+}
+
+// todo this should probably be quite reusable
+private fun chooseProjectFolderDialog(): String? {
+    // even though looks bad on windows, that's the only fast workable workaround
+    val f = JFileChooser()
+    f.currentDirectory = HomeFolder.toJavaFile()
+    f.dialogTitle = "New project folder"
+    f.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+    f.showDialog(null, "Choose project directory")
+    return f.selectedFile.absolutePath
 }
