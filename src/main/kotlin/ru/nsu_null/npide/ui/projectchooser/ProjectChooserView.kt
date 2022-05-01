fed2a810b310cbce997d5177cc612d5447f8a805
Roman <b111forjob@gmail.com>
2022-05-01 20:18:59 +0700

    Delete project button icon

--- src/main/kotlin/ru/nsu_null/npide/ui/projectchooser/ProjectChooserView.kt
+++ src/main/kotlin/ru/nsu_null/npide/ui/projectchooser/ProjectChooserView.kt
@@ -3,11 +3,10 @@ package ru.nsu_null.npide.ui.projectchooser
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.*
 import androidx.compose.material.ButtonDefaults.buttonColors
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -82,7 +81,9 @@ fun ProjectChooserView() {
                         Button(
                             onClick = { projectChooser.deleteProject(project) },
                             content = {
-                                Text("X")
+                                Icon(Icons.Default.DeleteForever,
+                                    tint = Color.White,
+                                    contentDescription = "Close")
                             },
                             colors = grayButtonColor
                         )
@@ -95,7 +96,7 @@ fun ProjectChooserView() {
 
 // todo this should probably be quite reusable
 private fun chooseProjectFolderDialog(): String? {
-    // even though looks bad on windows, that's the only fast workable workaround
+    // even though looks bad on Windows, that's the only fast workable workaround
     val f = JFileChooser()
     f.currentDirectory = HomeFolder.toJavaFile()
     f.dialogTitle = "New project folder"
