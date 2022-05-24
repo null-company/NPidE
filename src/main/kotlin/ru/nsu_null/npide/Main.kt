a62fda2f1fd673e223ee89a7858df87804cde114
Roman <b111forjob@gmail.com>
2022-05-24 18:22:53 +0700

    Fix fullscreen
    it is now clunky and slow but it is the point

--- src/main/kotlin/ru/nsu_null/npide/Main.kt
+++ src/main/kotlin/ru/nsu_null/npide/Main.kt
@@ -1,8 +1,8 @@
 package ru.nsu_null.npide
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.awt.ComposeWindow
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
@@ -11,9 +11,7 @@ import androidx.compose.ui.input.key.type
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.singleWindowApplication
+import androidx.compose.ui.window.*
 import ru.nsu_null.npide.ui.ProjectView
 import ru.nsu_null.npide.ui.menubar.CustomMenuBar
 import ru.nsu_null.npide.ui.npide.NPIDE
@@ -21,37 +19,87 @@ import ru.nsu_null.npide.ui.npide.NPIDE.State.CHOOSING_PROJECT
 import ru.nsu_null.npide.ui.npide.NPIDE.State.IN_PROJECT
 import ru.nsu_null.npide.ui.projectchooser.ProjectChooserView
 
+val icon = BitmapPainter(useResource("npide.png", ::loadImageBitmap))
+const val npideTitle = "NPidE"
+
 @ExperimentalComposeUiApi
 @ExperimentalFoundationApi
-fun main() {
+fun main() = application {
 
-    lateinit var windowCaptured: ComposeWindow
-    fun switchFullscreen() {
-        if (windowCaptured.placement == WindowPlacement.Fullscreen) {
-            windowCaptured.placement = WindowPlacement.Floating
-        } else {
-            windowCaptured.placement = WindowPlacement.Fullscreen
+    var isFullscreen by remember { mutableStateOf(false) }
+    val switchFullscreen = {
+        isFullscreen = !isFullscreen
+    }
+    val onClose = {
+        exitApplication()
+    }
+
+    if (isFullscreen) {
+        FullscreenWindow(onClose, switchFullscreen) {
+            NpideView()
+        }
+    } else {
+        FloatingWindow(onClose, switchFullscreen) {
+            NpideView()
         }
     }
 
-    singleWindowApplication(
-        title = "NPidE",
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun FloatingWindow(onCloseRequest: () -> Unit,
+                   onSwitchFullscreenRequest: () -> Unit,
+                   content: @Composable FrameWindowScope.() -> Unit) {
+    Window(
+        onCloseRequest = onCloseRequest,
+        title = npideTitle,
+        undecorated = false,
         state = WindowState(width = 1280.dp, height = 768.dp),
-        icon = BitmapPainter(useResource("npide.png", ::loadImageBitmap)),
+        icon = icon,
         onKeyEvent = { e ->
             if (e.key == Key.F11 && e.type == KeyEventType.KeyDown) {
-                switchFullscreen()
+                onSwitchFullscreenRequest()
                 true
             } else {
                 false
             }
         }
     ) {
-        windowCaptured = window
-        CustomMenuBar()
-        when(NPIDE.state) {
-            CHOOSING_PROJECT -> ProjectChooserView()
-            IN_PROJECT -> ProjectView()
+        content()
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun FullscreenWindow(onCloseRequest: () -> Unit,
+                     onSwitchFullscreenRequest: () -> Unit,
+                     content: @Composable FrameWindowScope.() -> Unit) {
+    Window(
+        onCloseRequest = onCloseRequest,
+        title = npideTitle,
+        undecorated = true,
+        state = WindowState(placement = WindowPlacement.Fullscreen),
+        icon = icon,
+        onKeyEvent = { e ->
+            if (e.key == Key.F11 && e.type == KeyEventType.KeyDown) {
+                onSwitchFullscreenRequest()
+                true
+            } else {
+                false
+            }
         }
+    ) {
+        content()
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun FrameWindowScope.NpideView() {
+    CustomMenuBar()
+    when(NPIDE.state) {
+        CHOOSING_PROJECT -> ProjectChooserView()
+        IN_PROJECT -> ProjectView()
     }
 }
