44eed1406df11a6068aa8ef35b476750d2767529
Roman <b111forjob@gmail.com>
2022-05-24 21:19:07 +0700

    Dummy regex reading for watches

--- src/main/kotlin/ru/nsu_null/npide/ui/console/watches/WatchesView.kt
+++ src/main/kotlin/ru/nsu_null/npide/ui/console/watches/WatchesView.kt
@@ -1,5 +1,6 @@
 package ru.nsu_null.npide.ui.console.watches
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
@@ -13,13 +14,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ru.nsu_null.npide.ui.GitBranchTellerView
+import ru.nsu_null.npide.ui.common.AppTheme
 import ru.nsu_null.npide.ui.console.Console
 
 @Composable
-fun WatchesView(modifier: Modifier, console: Console, onControlPanelSwitchRequest: () -> Unit, onCloseRequest: () -> Unit) {
+fun WatchesView(
+    modifier: Modifier,
+    console: Console,
+    onControlPanelSwitchRequest: () -> Unit,
+    onCloseRequest: () -> Unit
+) {
     Box(Modifier.fillMaxSize().padding(15.dp).then(modifier)) {
         Column(Modifier.fillMaxSize()) {
-            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+            Row(horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()) {
                 Row(horizontalArrangement = Arrangement.Start) {
                     if (console.processIsAttached) {
                         Icon(Icons.Default.Done, "Process is attached", tint = Color.Green)
@@ -39,7 +47,23 @@ fun WatchesView(modifier: Modifier, console: Console, onControlPanelSwitchReques
             }
             Divider(Modifier.padding(0.dp, 15.dp))
             Column(Modifier.fillMaxSize()) {
-                Text("THIS IS WATCHES", modifier = Modifier.weight(0.8f))
+                val watchesRegex = Regex("([^= ]*)=([^= ]*)")
+
+                val lastLinesN = 2
+                val lastLines = console.content.takeLast(lastLinesN).map { it.text }.reduceRight(String::plus)
+
+                println(watchesRegex.findAll(lastLines).count())
+                val res = watchesRegex.findAll(lastLines).map { it.value }.reduceOrNull { acc, s -> "$acc,$s" }
+
+                Box(
+                    Modifier
+                        .weight(0.8f)
+                        .fillMaxWidth()
+                        .background(AppTheme.colors.backgroundDark.copy(alpha = 0.5f))
+                ) {
+                    Text(res ?: "N/a", modifier = Modifier.padding(3.dp))
+                }
+                Spacer(Modifier.padding(2.dp))
                 GitBranchTellerView(Modifier.weight(0.2f))
             }
         }
