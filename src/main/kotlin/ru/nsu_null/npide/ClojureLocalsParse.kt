1298e3917c52a37b3836fbaf3d305f586cf8489b
Roman <42515597+ghB111@users.noreply.github.com>
2022-06-18 16:39:36 +0700

    code style

--- src/main/kotlin/ru/nsu_null/npide/ClojureLocalsParse.kt
+++ src/main/kotlin/ru/nsu_null/npide/ClojureLocalsParse.kt
@@ -1,47 +1,42 @@
 package ru.nsu_null.npide
 
-import java.util.*
-import kotlin.collections.HashMap
-
-fun parseLocals(str: String): HashMap<String, String> {
-    val string_literal = "\"([^\"\\\\]|\\\\.)*\""
-    val string_regex = Regex(string_literal)
-    val str_without_strings = string_regex.replace(str) {
+fun parseLocals(str: String): Map<String, String> {
+    val stringRegex = """"([^"\\]|\\.)*"""".toRegex()
+    val strWithoutStrings = stringRegex.replace(str) {
         "#".repeat(it.value.length)
     }
-    val getSubstring = { s: String, range: IntRange -> s.substring(range.start, range.endInclusive + 1) }
     var counter = 0
-    var prev_idx = -1
-    var vars = LinkedList<IntRange>();
+    var prevIdx = -1
+    val vars = mutableListOf<IntRange>()
     for (i in str.indices) {
         if (str[i] in "{([") {
-            counter++;
-            continue;
+            counter++
+            continue
         } else if (str[i] in "})]") {
-            counter--;
+            counter--
         }
         if (counter <= 1) {
-            if (prev_idx == -1) {
-                prev_idx = i;
-                continue;
+            if (prevIdx == -1) {
+                prevIdx = i
+                continue
             }
             if (str[i] in ",}" || i == str.length - 1) {
-                if(counter == 0){
-                    vars.add(IntRange(prev_idx, i-1))
-                }else{
-                    vars.add(IntRange(prev_idx, i))
+                if (counter == 0) {
+                    vars.add(prevIdx..i)
+                } else {
+                    vars.add(prevIdx..i + 1)
                 }
-                prev_idx = -1;
+                prevIdx = -1
             }
         }
     }
-    val stringVarsTrimmed = vars.map { getSubstring(str, it).trim { it in ", \n" } }
-    val nameToValue = HashMap<String, String>()
+    val stringVarsTrimmed = vars.map { variableRange -> str.substring(variableRange).trim { it in ", \n" } }
+    val nameToValue = mutableMapOf<String, String>()
     for (varStr in stringVarsTrimmed) {
         for (i in varStr.indices) {
             if (varStr[i] in "\n\r\t ") {
-                nameToValue[varStr.slice(IntRange(0, i - 1)).trim { it in " \n" }] =
-                    varStr.slice(IntRange(i + 1, varStr.length - 1)).trim { it in ", \n" }
+                nameToValue[varStr.slice(0 until i).trim { it in " \n" }] =
+                    varStr.slice(i + 1 until varStr.length).trim { it in ", \n" }
                 break
             }
         }
@@ -51,16 +46,17 @@ fun parseLocals(str: String): HashMap<String, String> {
 
 fun main() {
     val vars = parseLocals(
-        "{graph\n" +
-                " {:forward\n" +
-                "  {\"A\" {\"B\" {:price 200, :tickets #<Ref@dfb38f: 5>}},\n" +
-                "   \"B\" {\"C\" {:price 100, :tickets #<Ref@2be77417: 0>}}},\n" +
-                "  :backward\n" +
-                "  {\"B\" {\"A\" {:price 200, :tickets #<Ref@dfb38f: 5>}},\n" +
-                "   \"C\" {\"B\" {:price 100, :tickets #<Ref@2be77417: 0>}}}},\n" +
-                " g\n" +
-                " {\"A\" {\"B\" {:price 200, :tickets #<Ref@dfb38f: 5>}},\n" +
-                "  \"B\" {\"C\" {:price 100, :tickets #<Ref@2be77417: 0>}}}}\n"
+        """{graph
+ {:forward
+  {"A" {"B" {:price 200, :tickets #<Ref@dfb38f: 5>}},
+   "B" {"C" {:price 100, :tickets #<Ref@2be77417: 0>}}},
+  :backward
+  {"B" {"A" {:price 200, :tickets #<Ref@dfb38f: 5>}},
+   "C" {"B" {:price 100, :tickets #<Ref@2be77417: 0>}}}},
+ g
+ {"A" {"B" {:price 200, :tickets #<Ref@dfb38f: 5>}},
+  "B" {"C" {:price 100, :tickets #<Ref@2be77417: 0>}}}}
+"""
     )
     for ((key, value) in vars) {
         println("$key = $value")
